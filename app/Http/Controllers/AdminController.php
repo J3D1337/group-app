9cee936763765a9ba22a4381e6c6f431cef6cb3e
Marko <mmarko1g@gmail.com>
2024-06-27 18:20:08 +0200

    modify admin controller and navbar

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -9,7 +9,7 @@ class AdminController extends Controller
 //if admin@admin.com is loged in redirect do admin.dashboard if not redirect do users.dashboard
     public function dashboard()
     {
-        if (auth()->user()->email == 'admin@admin.com') {
+        if (auth()->user()->role == 'admin') {
             return view('admin.dashboard');
         } else {
             return view('users.dashboard');
---
+++ storage/framework/views/6b27a7a8c1f445393e17175c0b083a0a.php
@@ -0,0 +1,21 @@
+<div>
+    
+
+<?php $__env->startSection('title'); ?>
+    Dashboard
+<?php $__env->stopSection(); ?>
+
+<?php $__env->startSection('content'); ?>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h2>Dashboard</h2>
+                <p>Welcome to the dashboard</p>
+            </div>
+        </div>
+    </div>
+<?php $__env->stopSection(); ?>
+
+</div>
+
+<?php echo $__env->make('layout.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\laravel\group-app\resources\views/users/dashboard.blade.php ENDPATH**/ ?>
